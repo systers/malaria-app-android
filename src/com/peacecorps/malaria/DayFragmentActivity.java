38198ffe9d543281b2e9fc0cdc42bf74605b2582
Michaeldgeek <petermichael908@gmail.com>
2016-03-14 22:50:22 +0100

    Added values-fr for french language.

--- src/com/peacecorps/malaria/DayFragmentActivity.java
+++ src/com/peacecorps/malaria/DayFragmentActivity.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
