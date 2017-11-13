d6f18d2c898db383ba125009d041e3c316a080a5
sammy1997 <sombuddha2016@gmail.com>
2017-11-14 03:00:50 +0530

    Service to play the Alarm Tone

---
+++ malaria-app-android/src/main/java/com/peacecorps/malaria/services/RingService.java
@@ -0,0 +1,40 @@
+package com.peacecorps.malaria.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RingService extends Service {
+    private Ringtone ringtone;
+
+    public RingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Uri ringtoneUri = Uri.parse(intent.getExtras().getString("ringtone"));
+        Log.e("RingService OnStart","Starting Song");
+        this.ringtone = RingtoneManager.getRingtone(this, ringtoneUri);
+        ringtone.play();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.e("RingService OnEnd", "Stopping Song");
+        ringtone.stop();
+    }
+}
